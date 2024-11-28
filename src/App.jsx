e37fb1ddbf0cf6bb3b4aff6b75a1b60d3168ca28
Ahmad Ali <xevenahmad@gmail.com>
2024-11-29 00:00:17 +0500

    new compinent add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css';
-import ImageCropper from './components/ImageCropper';
-// import ImageCropperWithPreview from './components/ImageCropperWithPreview';
+import CropDemo from './components/ImageCropper';
 
 function App() {
+  const imageUrl = 'https://images.unsplash.com/photo-1439871846984-851e435a999b?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDN8NnNNVmpUTFNrZVF8fGVufDB8fHx8fA%3D%3D';
+
   return (
     <div>
-      <ImageCropper />
-      {/* <ImageCropperWithPreview/> */}
+      <h1>Image Crop Demo</h1>
+      <CropDemo src={imageUrl} />
     </div>
   );
 }
--- src/components/ImageCropper.jsx
+++ src/components/ImageCropper.jsx
@@ -1,46 +1,25 @@
-import  { useState } from 'react';
+/*eslint-disable*/
+
+import React, { useState } from 'react';
 import ReactCrop from 'react-image-crop';
 import 'react-image-crop/dist/ReactCrop.css';
 
-function ImageCropper() {
+function CropDemo({ src }) {
   const [crop, setCrop] = useState({
-    unit: '%', // Can be 'px' or '%'
-    width: 300, 
-    aspect: 16 / 9, // Aspect ratio
+    unit: 'px', // Units for the crop area (can be px or %)
+    width: 200, // Width of the crop area
+    aspect: 16 / 9, // Aspect ratio of the crop area
   });
-
-  const [src, setSrc] = useState(null);
-
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    const reader = new FileReader();
-
-    reader.onloadend = () => {
-      setSrc(reader.result);
-    };
-
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  };
-
-  const onCropChange = (newCrop) => {
-    setCrop(newCrop);
-  };
 
   return (
     <div>
-      <input type="file" onChange={handleImageChange} />
-      {src && (
-        <ReactCrop
-          src={src}
-          crop={crop}
-          onChange={onCropChange}
-          onComplete={(c) => console.log('Crop complete:', c)}
-        />
-      )}
+      <ReactCrop
+        src={src}
+        crop={crop}
+        onChange={(newCrop) => setCrop(newCrop)}
+      />
     </div>
   );
 }
 
-export default ImageCropper;
+export default CropDemo;
